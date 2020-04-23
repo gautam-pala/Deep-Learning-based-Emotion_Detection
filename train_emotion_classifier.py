9c75c0ac80f3562196990bc8bab901c4f2d94ad5
gautam-pala <51695969+gautam-pala@users.noreply.github.com>
2020-04-23 22:45:27 +0530

    Add files via upload

---
+++ train_emotion_classifier.py
@@ -0,0 +1,90 @@
+"""
+File: train_emotion_classifier.py
+Author: Gautam Pala
+Description: Train emotion classification model
+"""
+
+from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+
+from models.cnn import mini_XCEPTION
+from utils.datasets import DataManager
+from utils.datasets import split_data
+from utils.preprocessor import preprocess_input
+import wandb
+from wandb.wandb_keras import WandbKerasCallback
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--patience", type=int, default=50)
+parser.add_argument("--rotation_range", type=int, default=10)
+parser.add_argument("--width_shift_range", type=float, default=0.1)
+parser.add_argument("--height_shift_range", type=float, default=0.1)
+parser.add_argument("--zoom_range", type=float, default=0.1)
+parser.add_argument("--batch_size", type=int, default=32)
+parser.add_argument("--num_epochs", type=int, default=10000)
+
+args = parser.parse_args()
+
+
+
+run = wandb.init()
+config = run.config
+config.update(args)
+
+input_shape = (64, 64, 1)
+validation_split = .2
+verbose = 1
+num_classes = 7
+
+base_path = '../trained_models/emotion_models/'
+
+
+
+# data generator
+data_generator = ImageDataGenerator(
+                        featurewise_center=False,
+                        featurewise_std_normalization=False,
+                        rotation_range=config.rotation_range,
+                        width_shift_range=config.width_shift_range,
+                        height_shift_range=config.height_shift_range,
+                        zoom_range=config.zoom_range,
+                        horizontal_flip=True)
+
+# model parameters/compilation
+model = mini_XCEPTION(input_shape, num_classes)
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+
+datasets = ['fer2013']
+for dataset_name in datasets:
+    print('Training dataset:', dataset_name)
+
+    # callbacks
+    log_file_path = base_path + dataset_name + '_emotion_training.log'
+    csv_logger = CSVLogger(log_file_path, append=False)
+    early_stop = EarlyStopping('val_loss', patience=config.patience)
+    reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1,
+                                  patience=int(config.patience/4), verbose=1)
+    trained_models_path = base_path + dataset_name + '_mini_XCEPTION'
+    model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
+    model_checkpoint = ModelCheckpoint(model_names, 'val_loss', verbose=1,
+                                                    save_best_only=True)
+    wandb_callback = WandbKerasCallback()
+    callbacks = [model_checkpoint, csv_logger, early_stop, reduce_lr, wandb_callback]
+
+    # loading dataset
+    data_loader = DataManager(dataset_name, image_size=input_shape[:2])
+    faces, emotions = data_loader.get_data()
+    faces = preprocess_input(faces)
+    num_samples, num_classes = emotions.shape
+    train_data, val_data = split_data(faces, emotions, validation_split)
+    train_faces, train_emotions = train_data
+    model.fit_generator(data_generator.flow(train_faces, train_emotions,
+                                            config.batch_size),
+                        steps_per_epoch=len(train_faces) / config.batch_size,
+                        epochs=config.num_epochs, verbose=1, callbacks=callbacks,
+                        validation_data=val_data)
